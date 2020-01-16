6ef3972d5f6e53aeb4f4f4f92c5db55718981e2a
Qchmqs <achmas@live.fr>
2020-01-16 10:33:28 +0100

    added server code and form template

--- main.cr
+++ main.cr
@@ -1,6 +1,16 @@
 require "kemal"
 
-post "/" do |request|
+post "/" do |env|
+  # grab what the browser sent
+  a = env.params.body["a"]
+  b = env.params.body["b"]
+
+  # display it back to the browser
+  "a: #{a} \n
+  b: #{b}  \n
+
+  Total: #{a + b}"
 end
 
+Kemal.config.port = 3050
 Kemal.run
